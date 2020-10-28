928562da898cf5943c3e5ea6d868ab312ce31f4f
Gonzalo J. Carracedo Carballal <gonzalo.carracedo@tarlogic.com>
2020-10-28 08:41:35 +0100

    Add missing files to project

---
+++ CredentialsUI.h
@@ -0,0 +1,49 @@
+#ifndef CREDENTIALSUI_H
+#define CREDENTIALSUI_H
+
+#include <QWidget>
+#include <QDateTime>
+#include <Topology/Concentrator.h>
+#include <App/MeterInfo.h>
+
+class QSortFilterProxyModel;
+class CredInfoTableModel;
+
+namespace Ui {
+  class CredentialsUI;
+}
+
+class CredentialsUI : public QWidget
+{
+  Q_OBJECT
+
+  QVector<CredInfo> credList;
+  QVector<CredInfo> pendingList;
+
+  QSortFilterProxyModel *proxy = nullptr;
+  CredInfoTableModel *model = nullptr;
+
+  bool sortingEnabled = false;
+  bool adjusting = false;
+
+  void saveLog(QString path);
+  void connectAll(void);
+
+public:
+  explicit CredentialsUI(QWidget *parent = 0);
+  ~CredentialsUI();
+
+  void saveCreds(CredInfo const &);
+  void refreshCredentials(void);
+  void setSortingEnabled(bool);
+  void clear(void);
+
+public slots:
+  void onSaveAs(bool);
+  void onClear(bool);
+
+private:
+  Ui::CredentialsUI *ui;
+};
+
+#endif // CREDENTIALSUI_H
